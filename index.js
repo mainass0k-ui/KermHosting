1919130535424078d8e8e08d091856273d236cf7
Lord Kerm🧃. <hostcollab703@gmail.com>
2026-04-13 10:50:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const resend = new Resend(RESEND_CONFIG.apiKey);
 const PTERODACTYL_CONFIG = {
     url: 'https://panel.kermhosting.site',
     applicationApiKey: 'ptla_Txoudg9wQjBfupPIechR8qDrcotmFIICjGFj6AdvAdx',
-    clientApiKey: 'ptlc_GbX8yaoYt9TEA5hs7mQtwrJE7JoN6D71ydDw2M5Oeib'
+    clientApiKey: 'ptlc_GbX8yaoYt9TEA5hs7mQtwrJE7JoN6D71ydDw2M5Oeib',
 '
 };
 
