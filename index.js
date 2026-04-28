1b51e6bfd3a074fd372ce98b9bbfbd5af94130b5
Lord Kerm🧃. <hostcollab703@gmail.com>
2026-04-28 18:01:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8025,6 +8025,7 @@ app.get('/email-verification', (req, res) => res.sendFile(path.join(__dirname, '
 app.get('/support', (req, res) => res.sendFile(path.join(__dirname, 'public', 'support.html')));
 app.get('/transactions', (req, res) => res.sendFile(path.join(__dirname, 'public', 'transactions.html')));
 app.get('/bot', (req, res) => res.sendFile(path.join(__dirname, 'public', 'bot.html')));
+app.get('/my-bots', (req, res) => res.sendFile(path.join(__dirname, 'public', 'my-bots.html')));
 app.get('/maintenance', (req, res) => res.sendFile(path.join(__dirname, 'public', 'maintenance.html')));
 app.get('/server/:id', (req, res) => res.sendFile(path.join(__dirname, 'public', 'server', 'view.html')));
 app.get('/server/:id/files', (req, res) => res.sendFile(path.join(__dirname, 'public', 'server', 'files.html')));
